ba9fcd764c21257843fa5419f1224456744eabbe
hrehfeld <hauke@haukerehfeld.de>
2019-09-14 19:21:54 +0200

    chg: convert to library oriented code

--- color_segment.py
+++ color_segment.py
@@ -3,35 +3,47 @@ import numpy as np
 import os
 import sys
 
-image_filepath = sys.argv[1]
+def create_color_marked_mask(image):
+    image_hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 
-image = cv2.imread(image_filepath)
+    # Range for lower red
+    lower_red = np.array([0,120,70])
+    upper_red = np.array([10,255,255])
+    mask1 = cv2.inRange(image_hsv, lower_red, upper_red)
 
-image_hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
- 
-# Range for lower red
-lower_red = np.array([0,120,70])
-upper_red = np.array([10,255,255])
-mask1 = cv2.inRange(image_hsv, lower_red, upper_red)
- 
-# Range for upper range
-lower_red = np.array([170,120,70])
-upper_red = np.array([180,255,255])
-mask2 = cv2.inRange(image_hsv, lower_red, upper_red)
- 
-# Generating the final mask to detect red color
-mask = mask1 + mask2
+    # Range for upper range
+    lower_red = np.array([170,120,70])
+    upper_red = np.array([180,255,255])
+    mask2 = cv2.inRange(image_hsv, lower_red, upper_red)
 
-N = int(image.shape[0] * 0.0075)
-print(N)
-kernel = np.ones((N, N),np.uint8)
+    # Generating the final mask to detect red color
+    mask = mask1 + mask2
+    return mask
+    
 
-mask = cv2.dilate(mask, kernel, iterations=1)
+def segment_color_marked(image):
+    mask = create_color_marked_mask(image)
+
+    mask = dilate(mask)
+    cv2.imwrite('mask.jpg', mask)
+    #mask_inverted = cv2.bitwise_not(mask)
+
+    result = cv2.bitwise_and(image, image, mask=mask)
+    return result
+    
+
+def dilate(image):
+    radius = int(image.shape[0] * 0.0075)
+    print(radius)
+    kernel = np.ones((radius, radius),np.uint8)
+
+    image = cv2.dilate(image, kernel, iterations=1)
+    return image
 
 
-#mask_inverted = cv2.bitwise_not(mask)
+if __name__ == '__main__':
+    image_filepath = sys.argv[1]
+    image = cv2.imread(image_filepath)
+    result = segment_color_marked(image)
 
-res = cv2.bitwise_and(image, image, mask=mask)
-
-
-cv2.imwrite('out.jpg', res)
+    cv2.imwrite('out.jpg', result)
